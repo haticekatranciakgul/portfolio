c004bb5a3d2df692b8a7cb4399051a210ae8ef7b
Hatice AKGÜL <https://github.com/haticekatranciakgul/>
2024-10-16 17:38:38 +0300

    created pages

---
+++ my-portfolio/src/pages/Home.js
@@ -0,0 +1,31 @@
+import '../../src/App.css';
+import { Outlet } from "react-router-dom";
+import { ColorModeContext, useMode } from "../theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Navbar from '../components/Navbar';
+import Container from '@mui/material/Container';
+import { Typography } from '@mui/material';
+// import ScrollToTop from "react-scroll-to-top";
+
+
+function App() {
+    const [theme, colorMode] = useMode();
+    return (
+        <div className="App">
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Navbar>
+                    </Navbar>
+                    <Typography variant="h1" fontWeight={'bold'} margin={'20px'} >Portfolio</Typography>
+                    <Container>
+                        <Outlet />
+                        {/* <ScrollToTop smooth /> */}
+                    </Container>
+                </ThemeProvider>
+            </ColorModeContext.Provider>
+        </div>
+    );
+}
+
+export default App;
