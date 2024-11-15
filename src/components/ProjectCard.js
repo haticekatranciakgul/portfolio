64fc30ffe89858bcd14a84b9230dce463d998bd9
Hatice AKGÜL <katrancihatice@icloud.com>
2024-11-15 19:26:06 +0300

    deploy updt

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -11,81 +11,81 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectProjects, incrementFavoriteCount } from '../redux/projectSlice'; 
+import { selectProjects, incrementFavoriteCount } from '../redux/projectSlice';
 import { useTranslation } from 'react-i18next';
 import emailjs from 'emailjs-com';
 import Grid from '@mui/material/Grid2';
 
 function ProjectCard() {
-    const { t } = useTranslation(); 
-    const dispatch = useDispatch(); 
-    const projects = useSelector(selectProjects); 
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const projects = useSelector(selectProjects);
 
     const sendEmail = (projectTitle) => {
         emailjs.send('service_8ihennh', 'template_0gylyjr', {
             message: `Favorite button has been clicked for ${projectTitle} project!`,
-        }, 'wP4HyAXYbimySOdV1') 
-        .then((response) => {
-            console.log('Email sent successfully:', response.status, response.text);
-        }, (err) => {
-            console.error('Failed to send email:', err);
-        });
+        }, 'wP4HyAXYbimySOdV1')
+            .then((response) => {
+                console.log('Email sent successfully:', response.status, response.text);
+            }, (err) => {
+                console.error('Failed to send email:', err);
+            });
     };
 
     const handleFavoriteClick = (project) => {
-        dispatch(incrementFavoriteCount(project.id)); 
-        sendEmail(project.title); 
+        dispatch(incrementFavoriteCount(project.id));
+        sendEmail(project.title);
     };
     if (!projects || projects.length === 0) {
-        return <div>No projects available</div>; 
+        return <div>No projects available</div>;
     }
 
     return (
         <>
             {projects.map((project) => (
                 <Grid size={4}>
-                     <Card key={project.id} >
-                    <CardHeader
-                        avatar={
-                            <Avatar
-                                alt="profilepic"
-                                src={project.avatar}
-                                sx={{ width: 30, height: 30 }}
-                            />
-                        }
-                        action={
-                            <IconButton aria-label="settings">
-                                <MoreVertIcon />
+                    <Card key={project.id} >
+                        <CardHeader
+                            avatar={
+                                <Avatar
+                                    alt="profilepic"
+                                    src={project.avatar}
+                                    sx={{ width: 30, height: 30 }}
+                                />
+                            }
+                            action={
+                                <IconButton aria-label="settings">
+                                    <MoreVertIcon />
+                                </IconButton>
+                            }
+                            title={t(project.title)}
+                            subheader={project.date}
+                        />
+                        <CardMedia
+                            component="img"
+                            height="194"
+                            image={project.image}
+                            alt={t(project.title)}
+                        />
+                        <CardContent>
+                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                {t(project.description)}
+                            </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="add to favorites" onClick={() => handleFavoriteClick(project)}>
+                                <FavoriteIcon />
+                                {project.favoriteCount > 0 && <span>{project.favoriteCount}</span>}
                             </IconButton>
-                        }
-                        title={t(project.title)} 
-                        subheader={project.date}
-                    />
-                    <CardMedia
-                        component="img"
-                        height="194"
-                        image={project.image}
-                        alt={t(project.title)}
-                    />
-                    <CardContent>
-                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                            {t(project.description)}
-                        </Typography>
-                    </CardContent>
-                    <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites" onClick={() => handleFavoriteClick(project)}>
-                            <FavoriteIcon />
-                            {project.favoriteCount > 0 && <span>{project.favoriteCount}</span>}
-                        </IconButton>
-                        <IconButton aria-label="share" 
-                            onClick={() => window.open(project.url, '_blank')}>
-                            <GitHubIcon />
-                        </IconButton>
-                    </CardActions>
-                </Card>
+                            <IconButton aria-label="share"
+                                onClick={() => window.open(project.url, '_blank')}>
+                                <GitHubIcon />
+                            </IconButton>
+                        </CardActions>
+                    </Card>
 
                 </Grid>
-               
+
             ))}
         </>
     );
